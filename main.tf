cde50dc002e175470cc4bbb1fe64bac7758886f0
Michela Federico <federico.michela@gmail.com>
2024-10-19 11:07:37 +0100

    adding s3 bucket

--- main.tf
+++ main.tf
@@ -17,16 +17,28 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+# Provider configuration for AWS
 provider "aws" {
   region = "us-east-1"
 }
 
+# EC2 instance resource (your existing resource)
 resource "aws_instance" "app_server" {
   # ami           = "ami-0fff1b9a61dec8a5f" // aws
   ami           = "ami-0866a3c8686eaeeba" // linux
   instance_type = "t2.micro"
 
   tags = {
-    Name = var.instance_name
+    Name = var.ec2_instance_name
+  }
+}
+
+# S3 bucket resource
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.s3_bucket_name
+
+  tags = {
+    Name        = var.s3_instance_name
+    Environment = "Production"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,17 @@
-variable "instance_name" {
+variable "ec2_instance_name" {
   type = string                                 # The type of the variable, in this case a string
-  default = "TPEC2Instance"          # Default value for the variable
-  description = "EC2 instance name tag value"   # Description of what this variable represents
+  default = "TP_EC2_Instance"          # Default value for the variable
+  description = "Terraform Playground EC2 instance name tag value"   # Description of what this variable represents
+}
+
+variable "s3_instance_name" {
+  type = string                                 # The type of the variable, in this case a string
+  default = "TP_S3_Instance"          # Default value for the variable
+  description = "Terraform Playground S3 instance name tag value"   # Description of what this variable represents
+}
+
+variable "s3_bucket_name" {
+  type = string                                 # The type of the variable, in this case a string
+  default = "tp-s3-bucket"                    # Default value for the variable
+  description = "Terraform Playground S3 bucket name"   # Description of what this variable represents
 }
