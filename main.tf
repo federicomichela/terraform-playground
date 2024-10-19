9a9e4e5b076c76545a4f076b45e3bef012a5488a
Michela Federico <federico.michela@gmail.com>
2024-10-19 17:05:05 +0100

    removed ec2 instance + creating iam policies before s3 infa

--- main.tf
+++ main.tf
@@ -22,91 +22,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-provider "aws" {
-  alias  = "california"
-  region = "us-west-1"
-}
-
-# EC2 instance resource
-resource "aws_instance" "app_server" {
-  provider = aws.virginia
-  # ami           = "ami-0fff1b9a61dec8a5f" // aws
-  ami           = "ami-0866a3c8686eaeeba" // linux
-  instance_type = "t2.micro"
-
-  tags = {
-    Name        = var.s3_instance_name
-    Environment = "Production"
-  }
-}
-
-# S3 bucket for website
-resource "aws_s3_bucket" "website_bucket" {
-  provider = aws.virginia
-  bucket   = "${var.s3_bucket_name}${random_id.bucket_suffix.hex}"
-
-  tags = {
-    Name        = var.s3_instance_name
-    Environment = "Production"
-  }
-}
-
-# Separate resource for website configuration
-resource "aws_s3_bucket_website_configuration" "website_config" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.website_bucket.bucket
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
-# Bucket policy for public access
-resource "aws_s3_bucket_policy" "website_bucket_policy" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.website_bucket.id
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*"
-    }
-  ]
-}
-EOF
-}
-
-# Public access block for the bucket
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  provider          = aws.virginia
-  bucket            = aws_s3_bucket.website_bucket.id
-  block_public_acls = true
-  block_public_policy = false
-}
-
-# Bucket ownership controls
-resource "aws_s3_bucket_ownership_controls" "ownership_controls" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.website_bucket.id
-
-  rule {
-    object_ownership = "BucketOwnerEnforced"
-  }
-}
-
-# Random suffix to ensure unique bucket name
-resource "random_id" "bucket_suffix" {
-  byte_length = 4
-}
 
 # IAM policy for the infra-admin user, dynamically referencing the S3 bucket name
 resource "aws_iam_policy" "s3_bucket_policy" {
@@ -136,4 +51,72 @@ resource "aws_iam_policy" "s3_bucket_policy" {
 resource "aws_iam_user_policy_attachment" "attach_s3_policy" {
   user       = "infra-admin"  # Replace with your IAM user or role
   policy_arn = aws_iam_policy.s3_bucket_policy.arn
+}
+
+# S3 bucket for website
+resource "aws_s3_bucket" "website_bucket" {
+  provider = aws.virginia
+  bucket   = "${var.s3_bucket_name}${random_id.bucket_suffix.hex}"
+
+  tags = {
+    Name        = var.s3_instance_name
+    Environment = "Production"
+  }
+}
+
+# Separate resource for website configuration
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  provider = aws.virginia
+  bucket   = aws_s3_bucket.website_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+# Public access block for the bucket
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  provider          = aws.virginia
+  bucket            = aws_s3_bucket.website_bucket.id
+  block_public_acls = true
+  block_public_policy = false
+}
+
+# Bucket ownership controls
+resource "aws_s3_bucket_ownership_controls" "ownership_controls" {
+  provider = aws.virginia
+  bucket   = aws_s3_bucket.website_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+# Bucket policy for public access
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  provider = aws.virginia
+  bucket   = aws_s3_bucket.website_bucket.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*"
+    }
+  ]
+}
+EOF
+}
+
+# Random suffix to ensure unique bucket name
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "ec2_instance_name" {
-  type = string                                 # The type of the variable, in this case a string
-  default = "TP_EC2_Instance"          # Default value for the variable
-  description = "Terraform Playground EC2 instance name tag value"   # Description of what this variable represents
-}
-
 variable "s3_instance_name" {
   type = string                                 # The type of the variable, in this case a string
   default = "TP_S3_Instance"          # Default value for the variable
